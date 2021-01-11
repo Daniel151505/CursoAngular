5f7c136387cf630df74e22d32e17cc639fbe69ea
Daniel151505 <daniel150520011@gmail.com>
2021-01-11 00:09:55 -0500

    Agregando el Componente Persona a las Declaraciones

--- CursoAngular/src/app/app.module.ts
+++ CursoAngular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonasComponent } from './personas/personas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonasComponent
   ],
   imports: [
     BrowserModule,
