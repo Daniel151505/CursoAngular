b8ca0764f6a973ea3bd45f34ad707831bfcfad0c
Daniel151505 <daniel150520011@gmail.com>
2021-01-13 19:41:57 -0500

    Agregando Metodo agregarPersona

--- CursoAngular/src/app/personas/personas.component.ts
+++ CursoAngular/src/app/personas/personas.component.ts
@@ -12,14 +12,23 @@ export class PersonasComponent implements OnInit {
   ngOnInit(): void {
   }
 
+ //Variables
   nombre: string ='Daniel';
   apellido: string = ' Altamirano';
   edad: number = 15;
   deshabilitar= false;
+  mensaje='No se ha agregado ni una persona'
   
   //metodo getEdad
 getEdad() :number {
   return this.edad;
+}
+
+//Metodo agregarPersona
+agregarPersona(){
+
+  this.mensaje='Persona agregada'
+
 }
 
   
