10cc97279a9bd974c6c6781bb6db2a1bccf82db4
Daniel151505 <daniel150520011@gmail.com>
2021-01-13 20:33:14 -0500

    Agregando Metodo y haciendo conversión

--- CursoAngular/src/app/app.component.ts
+++ CursoAngular/src/app/app.component.ts
@@ -7,7 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Nuevo Usuario';
-
   // Creando nuevo atributo con su respectivo valor
   saludo = 'Saludos Usuario';
+  titulo='';
+
+//Agregando metodo 
+modificarTitulo(event: Event){
+//Haciendo conversión(<HTMLInputElement>event.target).value
+this.titulo= (<HTMLInputElement>event.target).value
+
+}
+
+
 }
