13825119b4135de2a3a851e5eac0c0cceb152046
Daniel151505 <daniel150520011@gmail.com>
2021-01-13 21:14:28 -0500

    Agregando Forms Module

--- CursoAngular/src/app/app.module.ts
+++ CursoAngular/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PersonasComponent } from './personas/personas.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { PersonasComponent } from './personas/personas.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
