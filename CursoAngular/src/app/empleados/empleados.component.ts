4ff25f62f74bbd3dcb3e6622c6f1b67415cf6b09
Daniel151505 <daniel150520011@gmail.com>
2021-01-14 20:04:07 -0500

    Agregando metodo Agregar Empleado

--- CursoAngular/src/app/empleados/empleados.component.ts
+++ CursoAngular/src/app/empleados/empleados.component.ts
@@ -12,12 +12,28 @@ export class EmpleadosComponent implements OnInit {
 
   ngOnInit(): void {
   }
+ //Declaración de Variable
+  titulos: string ='Listado de empleados'
 
-  titulos='Listado de empleados'
+  //Creando Arreglo
   personas: Persona[] = [
     new Persona('Jhon','Soto'),
     new Persona('Sara','Connor')
-  
   ];
+
+  //Declaración de Variables
+  apellidoInput: string = '';
+  nombreInput: string='';
+
+  //Metodo Agregar Empleado
+  agregarEmpleado(){
+
+  //Jalando datos de apellidoInput y nombreInput
+  let empleado1 = new Persona(this.nombreInput, this.apellidoInput)
+  
+  //Agregando los datos al arreglo personas
+  this.personas.push(empleado1)
+  }
+
 
 }
