5aa4afd0c445ad08481d3f83b162330618aee93a
Daniel151505 <daniel150520011@gmail.com>
2021-01-15 15:44:01 -0500

    Usando el servicio de Data Service

--- CursoAngular/src/app/empleados/empleados.component.ts
+++ CursoAngular/src/app/empleados/empleados.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoggingService } from '../LoggingService.service';
+import { personasService } from '../personas.service';
 import { Persona } from './empleados.model';
 
 @Component({
@@ -9,17 +10,19 @@ import { Persona } from './empleados.model';
 })
 export class EmpleadosComponent implements OnInit {
 
-  constructor(private loggingService:LoggingService) { }
+  constructor(private loggingService:LoggingService, private personasService:personasService ) { }
 
   ngOnInit(): void {
+    //Se inicializa el arreglo con el arreglo que esta en personas.service.ts
+    this.personas = this.personasService.personas
   }
  //Declaración de Variable
   titulos: string ='Listado de empleados'
 
   //Creando Arreglo
   personas: Persona[] = [
-    new Persona('Jhon','Soto'),
-    new Persona('Sara','Connor')
+    // new Persona('Jhon','Soto'),
+    // new Persona('Sara','Connor')
   ];
 
   //Declaración de Variables
@@ -29,15 +32,13 @@ export class EmpleadosComponent implements OnInit {
   //Metodo Agregar Empleado
   agregarEmpleado(){
 
-  //Jalando datos de apellidoInput y nombreInput
-  let empleado1 = new Persona(this.nombreInput, this.apellidoInput)
-  
-  //Usando el servicio de Logging Service y mandando dato
-  this.loggingService.enviarMernsajeConsola('Enviamos persona '+ empleado1.nombre)
-  
-  //Agregando los datos al arreglo personas
-  this.personas.push(empleado1)
+      //Jalando datos de apellidoInput y nombreInput
+      let empleado1 = new Persona(this.nombreInput, this.apellidoInput)
+      //Usando el servicio de Logging Service y mandando dato
+      this.loggingService.enviarMernsajeConsola('Enviamos persona '+ empleado1.nombre)
+      //Agregando los datos al arreglo empleado
+      //this.personas.push(empleado1)
+      this.personasService.agregarEmpleado(empleado1)
+
   }
-
-
 }
